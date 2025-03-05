24427246dce43054820a1cbdf9f63f76cda2275f
Anisha Tamne <anishatamne@gmail.com>
2025-03-05 20:10:56 +0530

    Card file

---
+++ Card.java
@@ -0,0 +1,27 @@
+// Name: Anisha Tamne
+// PRN: 23070126014
+// Batch: A1
+
+// Card.java - Represents a single playing card
+public class Card {
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
