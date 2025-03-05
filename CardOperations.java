ef8da0aa579c2fc96832cb9fb8e552c8869d4aa4
Anisha Tamne <anishatamne@gmail.com>
2025-03-05 20:17:36 +0530

    Constructor

--- CardOperations.java
+++ CardOperations.java
@@ -6,4 +6,24 @@ public class CardOperations {
     private ArrayList<Card> deck;
     private static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
     private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        // Constructor that initializes and creates the deck
+        public CardOperations() {
+            createDeck();
+        }
+
+        // a) Creates a deck of 52 cards
+        public void createDeck() {
+            deck = new ArrayList<>();
+            for (String suit : SUITS) {
+                for (String rank : RANKS) {
+                    deck.add(new Card(suit, rank));
+                }
+            }
+        }
+    
+
+
+
+
 }
