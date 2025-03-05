79313dd1b84da155c430023ba96c02398f74a0d9
Anisha Tamne <anishatamne@gmail.com>
2025-03-05 20:13:14 +0530

    Card Operations Class

---
+++ CardOperations.java
@@ -0,0 +1,9 @@
+
+// CardOperations.java - Manages deck and card-related operations
+import java.util.*;
+
+public class CardOperations {
+    private ArrayList<Card> deck;
+    private static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+}
